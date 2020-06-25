97c636fd6c218936ace8b2e6cc2f8b1dcc1b509d
millo kuzman <millokuzman@gmail.com>
2020-06-25 11:51:24 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.weatherbit.io/v2.0/current?lat=' + latitude + '&lon=' + longitude + '&key=7107b1c70bc247fbb63255131d950d42'
+    request({ url, json: true }, (error, { body }) => {
+        const {error: error1, data } = body
+        if (error) {
+        callback('Unable to connect to weather service!', undefined)
+    } else if (error1) {
+        callback('Unable to find location', undefined)
+    } else {
+        callback(undefined, data[0].weather.description + '. It is currently ' + data[0].temp + ' degrees out. There is a ' + data[0].precip  + '% chance of rain.')
+    }
+ })
+}
+
+module.exports = forecast
